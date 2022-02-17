38e69ea947222cfe02451e4359ec79f8932a8e8f
sanoopark <sanoopark@gmail.com>
2022-02-18 07:02:35 +0900

    Feat: 전역 스타일 및 테마 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,47 @@
 import {createGlobalStyle, ThemeProvider} from 'styled-components';
 import reset from 'styled-reset';
+import {lightTheme, darkTheme} from 'theme';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
   body {
+    transition: color 0.2s ease-in-out;
+    transition: background-color 0.2s ease-in-out;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    height: 100%;
+    min-height: 100vh;
+    background-color: #fff;
+    background-image: linear-gradient(#fff 90%, #73737d 145%);
+    font-size: 1.125rem;
+    line-height: 1.75rem;
+    font-family: NanumSquareRoundR, sans-serif;
+    color: #0a0c10;
+  }
+
+  @font-face {
+    font-family: NanumSquareRoundR, sans-serif;
+    src: url("/fonts/NanumSquareRoundR.ttf") format("truetype");
+  }
+
+  @font-face {
+    font-family: NanumSquareRoundB, sans-serif;
+    src: url("/fonts/NanumSquareRoundB.ttf") format("truetype");
   }
 `;
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-};
 
 export default function App({Component, pageProps}) {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={lightTheme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
