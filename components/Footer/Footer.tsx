f10b7c7f8f7a8b1b35684c7e71d09590359f4afd
sanoopark <sanoopark@gmail.com>
2022-02-19 01:19:13 +0900

    Feat: 포스트 이미지 크기 조정

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -29,14 +29,14 @@ const Wrapper = styled.footer`
   flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
-  margin-left: auto;
-  margin-right: auto;
+  margin: 4rem auto 0 auto;
   border-top: ${({theme}) => `1px solid ${theme.colors.muted}`};
-  padding: 4rem 0 5rem 0;
+  padding: 4rem 0;
   width: calc(100% - 3rem);
   max-width: 65rem;
   font-size: 1.125rem;
   line-height: 1.75rem;
+  opacity: 50%;
   color: ${({theme}) => theme.colors.muted};
 
   @media (min-width: 768px) {
